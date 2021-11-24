b952dee9f389cc9cf60d4108c0a9fd891cce8dc2
mywork12-prog <madynetflix@gmail.com>
2021-11-24 09:05:42 -0500

    choise added

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
-from utils import my_one
-
-print('============Welcome to our app=================')
+from utils import print_choice,getIdOptions
+from colorama import Fore, Back, Style
+print('============Welcome to our app=================\n')
 
 
 
 if __name__ == "__main__":
     while(True):
-        my_one()
+        print_choice()
         print('Enter your commands: ')
         x = input()
-        print('Hello, ' + x)
-        if(x == "exit"):
-            exit(1)
+        if isinstance(x, int):
+            choice = getIdOptions(x)
+            print("int",choice)
+        else:
+            if(x == "exit"):
+                exit(1)
+            else:
+                print(Fore.RED+"Enter valid commands")
+                print(Style.RESET_ALL)
+                continue
+        
--- utils.py
+++ utils.py
@@ -1,2 +1,12 @@
-def my_one():
-    return 0
+def print_choice():
+    conf_list = ["mysql","nginx"]
+    print("choose from below options/n")
+    for i in range(len(conf_list)):
+        print(i," --- ",conf_list[i])
+    print("for Exit type exit")
+
+def getIdOptions(index):
+    conf_list = ["mysql","nginx"]
+    return conf_list[index]
+
+

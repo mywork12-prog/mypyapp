f8e7eaefd7d99c7f2c241a5c2744cc17ad6ecbfc
mywork12-prog <madynetflix@gmail.com>
2021-11-24 00:40:35 -0500

    exit functionlity add

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("hello world")
+print('============Welcome to our app=================')
+
+
+
+if __name__ == "__main__":
+    while(True):
+        print('Enter your name:')
+        x = input()
+        print('Hello, ' + x)
+        if(x == "exit"):
+            exit(1)

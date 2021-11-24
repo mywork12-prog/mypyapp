bd7fb394e18ed408a3ded85badb637829d77653c
mywork12-prog <madynetflix@gmail.com>
2021-11-24 00:55:31 -0500

    utility module add

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+from utils import my_one
+
 print('============Welcome to our app=================')
 
 
 
 if __name__ == "__main__":
     while(True):
+        my_one()
         print('Enter your name:')
         x = input()
         print('Hello, ' + x)
